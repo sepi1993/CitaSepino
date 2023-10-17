15bb146f65439ac1bb6e0e3585e1b136c9068f1a
Sepideh <142756250+sepi1993@users.noreply.github.com>
2023-10-17 12:44:54 -0700

    Update Home.vb

--- Home.vb
+++ Home.vb
@@ -2,14 +2,14 @@ B4A=true
 Group=Default Group
 ModulesStructureVersion=1
 Type=Service
-Version2120
+Version3333
 @EndOfDesignText@
 #Region  Service Attributes 
 	#StartAtBoot: False
 	#ExcludeFromLibrary: True
 #End Region
 
-' اتست انجام شد3333
+' اتست اخر صورت گرفا=ت9999
 ' This is main process
 Sub Process_Globals
 	'These global variables will be declared once when the application starts.
