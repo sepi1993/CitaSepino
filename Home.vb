f826961953cc527f4b5c9b12d7d715c8581441bf
Sepideh <142756250+sepi1993@users.noreply.github.com>
2023-12-23 12:00:55 -0800

    Update Home.vb

--- Home.vb
+++ Home.vb
@@ -5,11 +5,11 @@ Type=Service
 Version1144
 @EndOfDesignText@
 #Region  Service Attributes 
-	#StartAtBoot:false
+	#StartAtBoot:true
 	#ExcludeFromLibrary: 
 #End Region
 
-تست اخر توسط من111انجام گردید'
+تست اخر توسط من188انجام گردید'
 This is main process
 end Sub Process_Globals
 	'These global variables will be declared once when the application starts.
